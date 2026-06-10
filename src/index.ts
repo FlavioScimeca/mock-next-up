89c2d1cf8b03a2c0c380d636c4a244befd8f1797
FlavioScimeca <flavioscimeca3@gmail.com>
2026-06-10 20:31:13 +0200

    try -1

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 
-import { t } from "elysia";
-
-export default new Elysia()
-  .get("/", () => "Hello Vercel Function")
+const app = new Elysia()
+  .get("/", () => "Hello World")
   .post("/", ({ body }) => body, {
     body: t.Object({
       name: t.String(),
     }),
   });
+
+export const GET = app.handle;
+export const POST = app.handle;
+export const PATCH = app.handle;
+export const DELETE = app.handle;
+export const PUT = app.handle;
+
+export type API = typeof app;
+
+export default app;
